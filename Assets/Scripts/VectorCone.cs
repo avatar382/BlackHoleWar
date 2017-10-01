b398e2dd1fd578a54348615c806339d2fcd14a2f
randompast <matteroflight@gmail.com>
2017-09-30 22:20:04 -0400

    stage

--- Assets/Scripts/VectorCone.cs
+++ Assets/Scripts/VectorCone.cs
@@ -6,8 +6,9 @@ public class VectorCone : MonoBehaviour {
 
 	public GameObject player;
 	public ParticleSystem ps;
-	public GameObject targets;
 	public float maxAngle;
+	public GameObject selected;
+	public bool isSelected = false;
 
 	// Use this for initialization
 	void Start () {
@@ -15,14 +16,26 @@ public class VectorCone : MonoBehaviour {
 
 	void iterate(Vector3 p, Vector3 dir){
 		float dirMag = Vector3.Magnitude (dir);
-		foreach (Transform t in targets.transform){
-			Vector3 tp = t.position;
+		GameObject[] gos = GameObject.FindGameObjectsWithTag ("Debris");
+		GameObject minAGO;
+		float minAngle = maxAngle;
+		foreach (GameObject g in gos){
+			Vector3 tp = g.transform.position;
 			Vector3 toTarget = tp - p;
 			float theta = 180 * Mathf.Acos( Vector3.Dot (dir, toTarget) / 
 				(Vector3.Magnitude(toTarget) * dirMag ) ) / Mathf.PI;
-			if (theta < maxAngle) {
-				t.gameObject.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+			if (theta < minAngle) {
+				minAngle = theta;
+				selected = g;
+				isSelected = true;
+//				g.gameObject.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+//				g.transform.position += toTarget * 0.01f;
 			}
+		}
+		if (isSelected) {
+			selected.GetComponent<Debris>().isSelected = true;
+			print("selected an object foO!OiUFASOIFOWAIU YBOIUDWA GOIWAD");
+			isSelected = false;
 		}
 	}
 
@@ -32,7 +45,19 @@ public class VectorCone : MonoBehaviour {
 		s.angle = maxAngle;
 		Vector3 p = player.transform.position;
 		Vector3 dir = player.transform.TransformDirection(Vector3.forward);
-		Debug.DrawRay (p, dir*4);
-		iterate (p, dir);
+//		Debug.DrawRay (p, dir*40);
+		if (!isSelected) {
+//			iterate (p, dir);
+		}
+		RaycastHit hit;
+		if ( Physics.Raycast(p, dir, out hit, 10.0f) ){
+			print (hit.transform.gameObject.tag);
+			if (hit.transform.gameObject.tag == "Debris") {
+				GameObject go = hit.transform.gameObject;
+				go.GetComponent<Debris>().wasHit = true;
+				Rigidbody rb = go.GetComponent<Rigidbody> ();
+				rb.AddForce (dir * 1f, ForceMode.Impulse);
+			}
+		}
 	}
 }
